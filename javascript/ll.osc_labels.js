fd60cbc7b75a619872780d7426711eadd2cd51f6
Joe Steccato <joe@joesteccato.com>
2024-12-18 18:09:59 -0500

    fix: control@ osc labels

--- javascript/ll.osc_labels.js
+++ javascript/ll.osc_labels.js
@@ -9,20 +9,18 @@ autowatch = 1;
 inlets = 1;
 outlets = 1;
 
-
 function cleanString(str, length) {
-  const matches = str.match(/[a-zA-Z]+|[0-9]+/g);
-  if (matches) {
-    const sliced = matches.slice(0, length);
-    const numberMatch = str.match(/[0-9]+$/);
-    if (numberMatch) {
-      return sliced.join('') + numberMatch[0];
+    const matches = str.match(/[a-zA-Z]+|[0-9]+/g);
+    if (matches) {
+        const sliced = matches.slice(0, length);
+        const numberMatch = str.match(/[0-9]+$/);
+        if (numberMatch) {
+            return sliced.join("") + numberMatch[0];
+        }
+        return sliced.join("");
     }
-    return sliced.join('');
-  }
-  return '';
+    return "";
 }
-
 
 function changeLabels(list1, list2) {
     // Split the input lists into arrays
@@ -30,27 +28,20 @@ function changeLabels(list1, list2) {
     var array1 = arguments[0].split(" ");
     var array2 = arguments[1].split(" ");
 
-    // Check if the arrays have the same length
-    if (array1.length === array2.length) {
-        // Create an empty array to store the output messages
-        var outputMessages = [];
+    // Iterate through the arrays and pair the items
+    for (var i = 1; i < array1.length; i++) {
+        if (array2.length >= i + 1) {
+            var osc_name = array1[i] + "_label";
 
-        // Iterate through the arrays and pair the items
-        for (var i = 1; i < array1.length; i++) {
-        	var osc_name = array1[i] + "_label";
+            var act_param = array2[i].split("::");
+            act_param.shift();
+            var act_name = cleanString(act_param[0], 3);
+            var par_name = cleanString(act_param[1], 3);
 
-            var act_param = array2[i].split("::")
-            act_param.shift()
-            var act_name = cleanString(act_param[0], 3)
-            var par_name = cleanString(act_param[1], 3)
-
-            var control_name = act_name + "." + par_name
+            var control_name = act_name + "." + par_name;
 
             // act_param = ['ho_st1', 'vol_slider']
             outlet(0, osc_name, control_name);
         }
-    } else {
-        post("ll_osc_labels.js Error: Input lists have different lengths.");
-        post();
     }
 }
