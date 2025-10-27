c240b59e49ccfe68afce6adc736c0a42a06479a2
Joe Steccato <joe@joesteccato.com>
2025-10-27 16:57:14 -0400

    clean up ll.enviread

--- javascript/ll.enviread.js
+++ javascript/ll.enviread.js
@@ -1,12 +1,17 @@
 outlets = 2;
-// var d = new Dict("environment");
+
 var stateDict = new Dict("ppoollstate");
 
 var actr = new Global("act_rep");
 
 var actingON = 0;
 var param_excludes = ["_actwindow", "act::title_menu", "presets"];
-var act_param_excludes = ["ho_st1act_menu", "ho_st1envi_menu", "ho_st1rec", "ho_st1audioON/OFF"];
+var act_param_excludes = [
+    "ho_st1act_menu",
+    "ho_st1envi_menu",
+    "ho_st1rec",
+    "ho_st1audioON/OFF",
+];
 var debugpost = 0;
 
 var new_blues_oldenvi;
@@ -21,16 +26,12 @@ let isopen, toopen;
 
 let buffers = null;
 
-// don't recall presets for envi folders
-const presetsIgnore = ["ho_st1"]
+const presetsIgnore = ["ho_st1"];
 
-const PARAMS_DELAY = 1000;      // delay 1000 ms
-const PARAMS_RUN_NUMBER = 2;    // run 2x
+const PARAMS_DELAY = 500; // delay 1000 ms
+const PARAMS_RUN_NUMBER = 2; // run 2x
 
 let paramsCount = 0;
-
-let isGettingBuffers = false;
-
 
 function debug_post(a) {
     debugpost = a;
@@ -42,27 +43,25 @@ function msg_dictionary(d) {
     buffers = null;
     dict = d;
 
-	outlet(0, dict.props.path);
+    outlet(0, dict.props.path);
 
     environment = dict.environment;
-    if(environment.buffer_host1){
+
+    // TODO: check for buffers loaded via pres_menu and do that first?
+    if (environment.buffer_host1 && environment.buffer_host1.ll_buffers) {
         buffers = environment.buffer_host1.ll_buffers;
     }
-
-	// let enviDict = new Dict("environment");
-	// enviDict.parse(JSON.stringify(environment));
-	// environment = dict.environment;
 
     loadActs();
 }
 
-// Canonical act order: ho_st1, then buffer_host1 (only once), then sorted others
+// act order: ho_st1, buffer_host1, then the rest...
 function canonicalActOrder(allKeys) {
     const hasHo = allKeys.includes("ho_st1");
     const hasBuffer = allKeys.includes("buffer_host1");
 
     const filtered = allKeys
-        .filter(k => k !== "ho_st1" && !/^buffer_host\d+$/.test(k)) // drop all buffer_hostN
+        .filter((k) => k !== "ho_st1" && !/^buffer_host\d+$/.test(k)) // drop all buffer_hostN
         .sort((a, b) => a.localeCompare(b));
 
     if (hasHo) filtered.unshift("ho_st1");
@@ -100,9 +99,15 @@ function loadActs() {
 
     if (debugpost > 1) {
         post(
-            "order:", order, "\n",
-            "toopen:", toopen, "\n",
-            "open:", isopen, "\n"
+            "order:",
+            order,
+            "\n",
+            "toopen:",
+            toopen,
+            "\n",
+            "open:",
+            isopen,
+            "\n"
         );
     }
 
@@ -117,19 +122,17 @@ function loadAct() {
         messnamed("ll_actload", toopen[0]);
         return;
     }
-    // outlet(0, "actsdone");
 
     // load buffers
-    if(buffers){
+    if (buffers) {
         const buffer_dict = new Dict("ll_buffers");
-        buffer_dict.parse(JSON.stringify(buffers))
-        messnamed("llenviread_loadbuffers", "bang")
+        buffer_dict.parse(JSON.stringify(buffers));
+        messnamed("llenviread_loadbuffers", "bang");
     }
-
-	loadParams();
+    loadParams();
 }
 
-function loadParams(){
+function loadParams() {
     messnamed("llenviread", 1);
     paramsCount = 0;
     params();
@@ -137,7 +140,7 @@ function loadParams(){
 
 function acting(c, i, o) {
     //response from an act when ready loaded
-    if (actingON == 1 && o == 1) {
+    if (actingON === 1 && o === 1) {
         actingON = 0;
         setloc(c + i);
         toopen = toopen.slice(1);
@@ -158,14 +161,13 @@ function setloc(a) {
 //##################################################################____params
 function params() {
     new_blues_oldenvi = 0;
-    outlet(0, "parameters... (run " + (paramsCount + 1) + ")")
+    outlet(0, "parameters... (run " + (paramsCount + 1) + ")");
 
     let keys = Object.keys(environment);
     //post("envi_params_keys",keys,"\n");
     for (let a of keys) {
-        if(paramsCount === 1 && a === "buffer_host1")
-            continue;
-        
+        if (paramsCount === 1 && a === "buffer_host1") continue;
+
         let par_keys = Object.keys(environment[a]);
         //post("envi_params_keys",par_keys,"\n");
         if (debugpost > 0)
@@ -190,7 +192,8 @@ function params() {
                                         par4
                                     );
                                 }
-                            } else setparam(a, p1 + "::" + p2 + "::" + p3, par3);
+                            } else
+                                setparam(a, p1 + "::" + p2 + "::" + p3, par3);
                         }
                     } else setparam(a, p1 + "::" + p2, par2);
                 }
@@ -199,44 +202,35 @@ function params() {
     }
 
     paramsCount += 1;
-    if(paramsCount < PARAMS_RUN_NUMBER){
+    if (paramsCount < PARAMS_RUN_NUMBER) {
         messnamed("llenviread_getparams", PARAMS_DELAY);
-        // var t = new Task(params, this);
-        // t.schedule(PARAMS_DELAY); // run once after 2000 ms
-    }else{
-        loadPresets()
-        messnamed("llenviread", 0)
-        if(environment.ho_st1["audioON/OFF"] === 1){
+    } else {
+        loadPresets();
+        messnamed("llenviread", 0);
+        if (environment.ho_st1["audioON/OFF"] === 1) {
             outlet(0, "dac~", 1);
         }
         outlet(0, "done!");
     }
 }
 
-function loadPresets(){
+// load presets files for "folder" environments
+function loadPresets() {
+    if (dict.props.type !== "folder") return;
+
+    outlet(0, "presets...");
+
     let keys = Object.keys(environment);
+    for (const i in keys) {
+        if (presetsIgnore.indexOf(keys[i]) > -1) continue;
 
-    // load presets files for "folder" environments
-    if (dict.props.type === "folder") {
-        outlet(0, "presets...");
+        const filepath = `${dict.props.path}/presets/${keys[i]}.json`;
+        const f = new File(filepath, "read");
 
-        for (const i in keys) {
-            if (presetsIgnore.indexOf(keys[i]) > -1)
-                continue;
-
-            const filepath = `${dict.props.path}/presets/${keys[i]}.json`;
-            const f = new File(filepath, "read");
-
-            if (f.isopen) {
-                // File exists
-                f.close();
-                outlet(1, "send", `::${keys[i]}::pat`);
-                outlet(1, "read", filepath);
-            } 
-            // else {
-            //     // File not found... skip
-            //     post("No file: " + filepath + "\n");
-            // }
+        if (f.isopen) {
+            f.close();
+            outlet(1, "send", `::${keys[i]}::pat`);
+            outlet(1, "read", filepath);
         }
     }
 }
@@ -245,122 +239,15 @@ function setparam(a, p, v) {
     //act, param, value
     if (!param_excludes.includes(p) && !act_param_excludes.includes(a + p)) {
         if (debugpost > 1) post("parameter___", p, "####", v, "\n");
-        if (!import_old_llblues(a, p, v)) {
-            if (v[0] == "dictionary") senddict(a, p, v);
-            else messnamed(a, p, v);
-        }
-        //post(a,p,v,"\n");
-    }
-}
 
-function import_old_llblues(a, p, v) {
-    /*
-		state_old: 
-			1: output-channel-count (1-?)
-			2: show dark-blue / show light-blue (0,1) mix_adds (2,3)
-			3: volume layout (0, 1, 2)
-			4: meter (0,1,2)
-			5: inputs-channels-count (1-?)
-			6: link input-channels/output-channels (0,1)
-
-		status_new: 
-			1: 0: basic, 1: basic_in_mix, 2: mc.basic, 3: mc+chan_out
-			2: show outputs~/outputsMix~ (0,1)
-			3: volume layout (0, 1, 2)
-			4: meter (0,1,2)
-			5: mix_adds (0,1)
-			6: link input-channels to output-channels (0,1)
-			7: unfolded/folded
-	*/
-    try{
-        if (p == "ll.blues::outputs") {
-            check_newblues(a);
-            if (new_blues_oldenvi) {
-                old_outputs = v;
-                post("import old ll.blues, outputs:", v, "\n");
-                return 1;
-            } else return 0;
-        } else if (p == "ll.blues::state") {
-            messnamed(a, "v8", "getnamed", new_blues_name);
-            let get_status = actr.object
-                .subpatcher()
-                .getnamed("status")
-                .getvalueof();
-            let new_status = [
-                get_status[0],
-                v[1] % 2,
-                v[2],
-                v[3],
-                parseInt(v[1] / 2),
-                v[5],
-				0
-            ];
-            //post("new_state",new_state,"\n");
-            messnamed(a, new_blues_name + "::status", new_status);
-            messnamed(a, new_blues_name + "::chans", v[4], v[0]);
-            let out = [old_outputs[0]];
-            let outM = [old_outputs[1]];
-            for (i = 1; i < v[0]; i++) {
-                out.push("_");
-                outM.push("_");
-            }
-            //post("out",old_outputs[0]+out_,"outMix",old_outputs[1]+out_);
-            messnamed(a, new_blues_name + "::outputs~", out);
-            messnamed(a, new_blues_name + "::outputsMix~", outM);
-            return 1;
-        } else if (p == "outputs~") {
-            check_newblues(a);
-            if (new_blues_oldenvi) {
-                messnamed(a, "v8", "getnamed", "outputs~");
-                messnamed(a, "v8", "Getpatcher");
-                if (actr.object) {
-                    actr.patcher.remove(actr.object);
-                    post("removed old outputs~ and set into new ll.blues", "\n");
-                }
-                //post(new_blues_name+"::outputs~",v,"\n")
-                messnamed(a, new_blues_name + "::outputs~", v);
-            }
-        } else 
-        return 0;
-    }catch(e){
-        error("ll.enviread.js could not import old ll.blues\n\t", a, p, v, "\n\t", e.message, "\n")
-        return 0;
+        if (v[0] == "dictionary") senddict(a, p, v);
+        else messnamed(a, p, v);
     }
-    
-}
-
-function check_newblues(a) {
-    new_blues_name = 0;
-    actr.object = 0;
-    let blues_o = 0;
-    messnamed(a, "v8", "getnamed", "llblues");
-    if (actr.object) {
-        blues_o = actr.object;
-        new_blues_name = "llblues";
-        //post("new_blues_name",new_blues_name);
-    } else {
-        messnamed(a, "v8", "getnamed", "ll.blues");
-        if (actr.object) {
-            blues_o = actr.object;
-            new_blues_name = "ll.blues";
-            //post("new_blues_name2",new_blues_name);
-        } else {
-            post("no object called ll.blues or llblues \n");
-        }
-    }
-    if (blues_o){
-        try{
-            new_blues_oldenvi = blues_o.subpatcher().getnamed("outputs~") != 0;
-        }catch(e){
-            post(e, "\n")
-        }
-    }
-    //post("newblues",new_blues_oldenvi,"\n");
 }
 
 function checkdict(p) {
-    return typeof p == "object" && !Array.isArray(p);
     //the only way i found for checking if it is another dict
+    return typeof p === "object" && !Array.isArray(p);
 }
 
 function senddict(a, p, v) {
