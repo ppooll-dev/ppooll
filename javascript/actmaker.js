f3bc94cef9fa59dca225cad0d216594ce898cb9c
klausfilip <37405478+klausfilip@users.noreply.github.com>
2025-11-25 19:27:08 +0100

    actmaker new and change of active-setting in ll.act_main.js

---
+++ javascript/actmaker.js
@@ -0,0 +1,22 @@
+outlets = 1;
+
+function bpatch(a,b){
+    let tpp = this.patcher.parentpatcher;
+    if(!tpp){
+        post("actmaker -- no parentpatcher!", "\n");
+        return;
+    }
+    
+    if(this.patcher.box){
+        // in some patches, actmaker object already has varname "act"
+        this.patcher.box.varname = "actmaker"; 
+        post("old actmaker replaced in act",a,"\n");
+    }
+    
+	let act = tpp.newdefault(40,80,"bpatcher","@name", "act.maxpat", "@args", a, b);
+	act.varname = "act";
+    this.patcher.message("dispose")
+    // outlet(0,"dispose");
+}
+
+
