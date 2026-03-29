d8f527137dba16a8facf2c8e5990ffcbf0f47545
Joe Steccato <joe@joesteccato.com>
2026-03-29 12:56:46 -0400

    add timeformat back to ho_st preferences

--- javascript/ho_st/ll.preferences.js
+++ javascript/ho_st/ll.preferences.js
@@ -24,7 +24,8 @@ var empty_prf_DEFAULT = {
         flop_disable_cursor: 0,
         host_channels: 2,
         sort_envi_by: "name", // ["name", "recent"]
-        envi_history: []
+        envi_history: [],
+        time_format: "24hr"
     },
     file_paths: {
         quickrecord_path: 0,
@@ -306,6 +307,19 @@ function set_sort_envi_by(c) {
     ll_prf_rewrite();
 }
 
+var host_timeformat = "24h";
+declareattribute("host_timeformat", {
+    style: "enum",
+    enumvals: ["24h", "12h"],
+    setter: "sethost_timeformat",
+});
+function sethost_timeformat(c) {
+    host_timeformat = c;
+    preferences.set("general::time_format", c);
+    messnamed("ll_time_format", c);
+    ll_prf_rewrite();
+}
+
 
 var liveppooll_nested = 0;
 declareattribute("liveppooll_nested", { style: "onoff", setter: "set_liveppooll_nested" });
@@ -383,6 +397,11 @@ function readfile() {
 
     sort_envi_by = preferences.get("general::sort_envi_by");
     this.patcher.getnamed("attrui_sort_envi_by").message("attr", "sort_envi_by");
+
+    host_timeformat = preferences.get("general::time_format");
+    this.patcher
+        .getnamed("attrui_htf")
+        .message("attr", "host_timeformat");
 
     messnamed("ll_preferences_ready", "bang");
 }
